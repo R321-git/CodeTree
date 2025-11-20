0c22e97afeb7afbbe6c4c30a8a43fb555a5bdf6e
R321-git <63543865+R321-git@users.noreply.github.com>
2025-11-21 00:09:48 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251121/윤년인가/is-leap-year.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int Y;
+    scanf("%d", &Y);
+
+    if(Y % 4 == 0 && !((Y % 100 == 0 && !(Y % 400 == 0))))
+    printf("true");
+    else
+    printf("false");
+
+    return 0;
+}
